ccae8fc2ec8d065a149a8fd32883dc472908ee29
Eduardo Ribeiro <eduardo.ribeiro@basis.com.br>
2022-03-31 13:16:44 -0300

    Criando um endpoint de um novo usuario

--- support-portal/src/main/java/com/api/supportportal/resource/UserResource.java
+++ support-portal/src/main/java/com/api/supportportal/resource/UserResource.java
@@ -1,18 +1,26 @@
 package com.api.supportportal.resource;
 
+import com.api.supportportal.domain.User;
 import com.api.supportportal.exception.domain.EmailExistException;
 import com.api.supportportal.exception.ExceptionHandling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.supportportal.exception.domain.UserNotFoundException;
+import com.api.supportportal.exception.domain.UsernameExistException;
+import com.api.supportportal.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(path = {"/","/user"})
 public class UserResource extends ExceptionHandling {
 
-    @GetMapping("/home")
-    public String showUser() throws EmailExistException{
-        throw new EmailExistException(("This email address is already taken"));
-      // return "application works";
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
+        User newUser = userService.register(user.getFistName(), user.getLastName(), user.getUsername(), user.getEmail());
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
    }
 }
