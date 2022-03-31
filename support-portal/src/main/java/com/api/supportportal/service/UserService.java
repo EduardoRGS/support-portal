c72448798b59fba6aabefd779fd7ed9be0b95eed
Eduardo Ribeiro <eduardo.ribeiro@basis.com.br>
2022-03-31 13:16:44 -0300

    Teste dCriando um nendpoint de cadastro doe um novo usuário

--- support-portal/src/main/java/com/api/supportportal/service/UserService.java
+++ support-portal/src/main/java/com/api/supportportal/service/UserService.java
@@ -1,7 +1,20 @@
 package com.api.supportportal.service;
 
+import com.api.supportportal.domain.User;
+import com.api.supportportal.exception.domain.EmailExistException;
+import com.api.supportportal.exception.domain.UserNotFoundException;
+import com.api.supportportal.exception.domain.UsernameExistException;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
 public interface UserService {
+
+    User register(String fistName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException;
+
+    List<User> getAllUsers();
+
+    User findByUsername(String username);
+
+    User findUserByEmail(String email);
 }
