08088fb43bef42f29b778eac6161cc65f254b610
Eduardo Ribeiro <eduardo.ribeiro@basis.com.br>
2022-04-07 12:44:48 -0300

    Adicionando constante e service que envia uma notificação de email com a senha de um novo usuário

---
+++ support-portal/src/main/java/com/api/supportportal/service/EmailService.java
@@ -0,0 +1,53 @@
+package com.api.supportportal.service;
+
+import com.sun.mail.smtp.SMTPTransport;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+import static com.api.supportportal.constant.EmailConstant.*;
+import static javax.mail.Message.RecipientType.TO;
+import static javax.mail.Message.RecipientType.CC;
+
+@Service
+public class EmailService {
+
+    // send new password by new email for de user
+    public void sendNewPasswordEmail(String fistName, String password, String email) throws MessagingException {
+        Message message = createMail(fistName, password, email);
+        SMTPTransport smtpTransport = (SMTPTransport) getEmailSession().getTransport(SIMPLE_AIL_TRANSFER_PROTOCOL);
+        smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
+        smtpTransport.sendMessage(message, message.getAllRecipients());
+        smtpTransport.close();
+    }
+
+    // Create a message to user receive, after create a new email
+    private Message createMail(String fistName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(TO, InternetAddress.parse(email, false));
+        message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello" + fistName + ", \n \n Your new account password is: " + password + "\n \n The Support Team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
+
+    // Configuration the session for create email
+    private Session getEmailSession(){
+        Properties properties = System.getProperties();
+        properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
+        properties.put(SMTP_AUTH, true);
+        properties.put(SMTP_PORT, DEFAULT_PORT);
+        properties.put(SMTP_STARTTLS_ENABLE, true);
+        properties.put(SMTP_STARTTLS_REQUIRED, true);
+        return Session.getInstance(properties, null);
+    }
+}
