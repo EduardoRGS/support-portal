475ac305861dfcbb56e065179b1520520bc5b67a
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-04-11 20:39:22 -0300

    Adicionando o endpoint da criação de um novo usuário

--- support-portal/src/main/java/com/api/supportportal/resource/UserResource.java
+++ support-portal/src/main/java/com/api/supportportal/resource/UserResource.java
@@ -16,8 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+
+import java.io.IOException;
 
 import static com.api.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
 
@@ -46,8 +49,24 @@ public class UserResource extends ExceptionHandling {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException {
+    public ResponseEntity<User> registerUser(@RequestBody User user)
+            throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException {
         User newUser = userService.register(user.getFistName(), user.getLastName(), user.getUsername(), user.getEmail());
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+   }
+
+   @PostMapping("/add")
+    public ResponseEntity<User> addNewUser(@RequestParam("fistName") String fistName,
+                                           @RequestParam("lastName") String lastName,
+                                           @RequestParam("username") String username,
+                                           @RequestParam("email") String email,
+                                           @RequestParam("role") String role,
+                                           @RequestParam("isActive") String isActive,
+                                           @RequestParam("isNotLocked") String isNotLocked,
+                                           @RequestParam( value = "profileImage", required = false) MultipartFile profileImage)
+           throws UserNotFoundException, EmailExistException, IOException, UsernameExistException {
+        User newUser = userService.addNewUser(fistName, lastName, username, email, role,
+                Boolean.parseBoolean(isActive), Boolean.parseBoolean(isNotLocked), profileImage);
         return new ResponseEntity<>(newUser, HttpStatus.OK);
    }
 
