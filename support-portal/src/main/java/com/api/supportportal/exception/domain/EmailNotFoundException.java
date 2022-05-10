b2cdc2f61ab42310b3faacb2d90bcb5e71a31235
Eduardo Ribeiro <78619804+EduardoRGS@users.noreply.github.com>
2022-05-10 19:06:21 -0300

    Delete support-portal directory

--- support-portal/src/main/java/com/api/supportportal/exception/domain/EmailNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.api.supportportal.exception.domain;
-
-public class EmailNotFoundException extends Exception{
-
-    public EmailNotFoundException(String message){
-        super(message);
-    }
-}
